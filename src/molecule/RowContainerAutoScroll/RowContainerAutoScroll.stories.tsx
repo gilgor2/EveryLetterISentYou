ea8d557082cdfc4bb1f3bcc4facb8b6159b423cd
gilgor2 <misby0327@gmail.com>
2024-01-26 13:20:00 +0900

    test(molecule): molecule컴포넌트 테스트 작성

---
+++ src/molecule/RowContainerAutoScroll/RowContainerAutoScroll.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+import RowContainerAutoScroll from './RowContainerAutoScroll';
+
+const meta: Meta<typeof RowContainerAutoScroll> = {
+	title: 'molecule/RowContainerAutoScroll/RowContainerAutoScroll.stories.tsx',
+	tags: ['autodocs'],
+	component: RowContainerAutoScroll,
+};
+
+export default meta;
+type Story = StoryObj<typeof RowContainerAutoScroll>;
+
+export const Default: Story = {
+	args: { children: <div>aaa</div> },
+};
+
+export const FastOnHover: Story = {
+	args: { children: <div>aaa</div>, isFastOnHover: true },
+};
